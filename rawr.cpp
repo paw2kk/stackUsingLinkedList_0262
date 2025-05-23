109bbaeabe549a029355abc2cc479759169e0fe3
paw2kk <wndxakk@gmail.com>
2025-05-23 08:06:52 +0700

    membuat void peek untuk melihat data

--- rawr.cpp
+++ rawr.cpp
@@ -43,6 +43,22 @@ class Stack{
             cout << "Popped value: " << top->data << endl;
             delete temp;
          }
+
+         void peek(){
+            if (top == NULL)
+            {
+                cout << "List is empty." << endl;
+            }
+            else{
+                Node *current = top;
+                while (current != NULL){
+                    cout << current->data << " " << endl;
+                    current = current->next;
+                }
+                cout << endl;
+            }
+         }
+         
 }
 
 int main(){
