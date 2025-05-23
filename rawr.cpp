0ecf5cbee1c7c330d32552ca7357a0257e2233ff
paw2kk <wndxakk@gmail.com>
2025-05-23 08:24:52 +0700

    membuat case 3 yaitu stack peek

--- rawr.cpp
+++ rawr.cpp
@@ -93,5 +93,13 @@ int main(){
             cout << "Stack is empty. Cannot pop." << endl;
         }
         break;
+        case 3:
+        if (!stack.isEmpty())
+        {
+            stack.peek();
+        }
+        else{
+                
+        }
     }
 }
