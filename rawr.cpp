fd4a20c9d0458dbe8ffaf318ac333bfbd220a901
paw2kk <wndxakk@gmail.com>
2025-05-23 08:22:13 +0700

    membuat case 2

--- rawr.cpp
+++ rawr.cpp
@@ -84,6 +84,13 @@ int main(){
         cin >> value;
         stack push(value);
         break;
-        
+        case 2:
+        if (!stack.isEmpty())
+        {
+            stack.pop();
+        }
+        else{
+            
+        }
     }
 }
