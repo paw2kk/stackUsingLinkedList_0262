0cb64eeaed7521412f60864a61bbe7d34ee20a5a
paw2kk <wndxakk@gmail.com>
2025-05-23 07:52:35 +0700

    membuat variabel dan fungsi

--- rawr.cpp
+++ rawr.cpp
@@ -22,8 +22,16 @@ class Stack{
     Stack(){
         top + NULL; //Initialize the stack with a null top pointer
     }
+    //Push operation: Insert an element onto the top of the stack
+    int push(int value){
+        Node *newNode = new Node(); //1. Allocate memory for thew node
+        newNode->data = value;      //2. assign the value
+        newNode->next = top;        //3. set the next pointer of the new node to
+        top = newNode;              //4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
+        }
 
-    
 }
 
 int main(){
